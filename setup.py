c8831b1a49a04cf3e51820235d7957f30d8a9d12
yanom <makoto.yano@jp.fujitsu.com>
2020-02-04 09:25:05 +0900

    Change directory structure

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ __author__ = 'makoto yano'
 
 setup(
     name='dynamic_ansible_inventory',  # ライブラリの名前
-    version='0.1',  # バージョン
+    version='0.2',  # バージョン
     description='create inventory file for ansible',  # 解説
     author='makoto yano',  # 作った人
     author_email='yano.makoto20@gmail.com',  # 作った人の連絡先
@@ -18,7 +18,7 @@ setup(
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3 :: Only',
     ],
-    packages=find_packages(),  # 梱包するコードの場所. setuptoolsのfind_packages()でパッケージなdirectoryを捜索&よしなに突っ込んでくれます
+    packages=['dynamic_ansible_inventory'],  # 梱包するコードの場所. setuptoolsのfind_packages()でパッケージなdirectoryを捜索&よしなに突っ込んでくれます
     include_package_data=True,  # ソースコード(*.py)以外のファイルも入れるか否か
     keywords=['ansible', 'inventory'],  # キーワード
     license='Apache Software License',  # ライセンス
